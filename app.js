1947e08ae55900bf3715d3addd70c3b0bd5b5607
blakereeves19 <blake.reeves19@gmail.com>
2019-01-16 11:51:03 -0600

    Completed lab

--- app.js
+++ app.js
@@ -36,6 +36,11 @@ class Rectangle extends Shape {
         this.div.click(() => {
             this.describe();
         });
+
+        this.div.dblclick(() => {
+            this.div.remove();
+        })
+
     }
 
     describe() {
@@ -74,6 +79,10 @@ class Square extends Shape {
         this.div.click(() => {
             this.describe();
         });
+
+        this.div.dblclick(() => {
+            this.div.remove();
+        })
 
     }
 
@@ -114,6 +123,11 @@ class Circle extends Shape {
         this.div.click(() => {
             this.describe();
         });
+
+        this.div.dblclick(() => {
+            this.div.remove();
+        })
+
     }
 
     describe() {
@@ -127,6 +141,53 @@ class Circle extends Shape {
         navWidth.text(` ${this.width}`);
         navHeight.text(` ${this.height}`);
         navRadius.text(` ${this.radius}`);
+        navArea.text(` ${this.area}`);
+        navPerimeter.text(` ${this.perimeter}`);
+    }
+}
+
+class Triangle extends Shape {
+    constructor(shapeName, top, left, width, height, area, perimeter) {
+
+        super(width, height);
+
+        this.shapeName = shapeName;
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+        this.area = area;
+        this.perimeter = perimeter;
+        this.div = $(`<div class="${this.shapeName}"></div>`);
+        this.div.css({
+            "top": `${this.top}px`,
+            "left": `${this.left}px`,
+            "width": `0px`,
+            "height": `0px`,
+            "border-top": `${this.height}px solid yellow`,
+            "border-right": `${this.height}px solid transparent`
+        });
+        canvas.append(this.div);
+
+        this.div.click(() => {
+            this.describe();
+        });
+
+        this.div.dblclick(() => {
+            this.div.remove();
+        })
+
+    }
+
+    describe() {
+        let navShapeName = $(`#navShapeName`);
+        let navWidth = $(`#navWidth`);
+        let navHeight = $(`#navHeight`);
+        let navArea = $(`#navArea`);
+        let navPerimeter = $(`#navPerimeter`);
+        navShapeName.text(` ${this.shapeName}`);
+        navWidth.text(` ${this.width}`)
+        navHeight.text(` ${this.height}`);
         navArea.text(` ${this.area}`);
         navPerimeter.text(` ${this.perimeter}`);
     }
@@ -210,6 +271,25 @@ cirBtn.click(() => {
 
     console.log(newCir);
 });
-// let s1 = new Shape('Square', null, null, 40, 200, 50, null);
-// console.log(s1);
-// s1.describe();
+
+let triBtn = $(`#triBtn`);
+
+triBtn.click(() => {
+    this.triHeightInput = $(`#triHeightInput`).val();
+
+    let width = this.triHeightInput;
+    let height = this.triHeightInput;
+    let top = Math.floor(Math.random() * (600 - height));
+    let left = Math.floor(Math.random() * (600 - width));
+    let area = (height * width * 0.5);
+    let perimeter = ((height * 2) + (Math.sqrt(2) * height));
+
+    if (triHeightInput > 600) {
+        alert('The maximum input is 600. Try again!')
+        return;
+    }
+
+    let newTri = new Triangle('triangle', top, left, width, height, area, perimeter);
+
+    console.log(newTri);
+});
